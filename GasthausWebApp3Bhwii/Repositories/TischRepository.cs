3c38eb492005aeaadbc738a6c1e473741743838d
Felix-Leopold <leopoldfelix1@gmail.com>
2024-06-13 10:44:18 +0200

    tische hinzugefügt

--- GasthausWebApp3Bhwii/Repositories/TischRepository.cs
+++ GasthausWebApp3Bhwii/Repositories/TischRepository.cs
@@ -46,7 +46,7 @@ public class TischRepository
     {
         NpgsqlConnection myConnection = ConnectToDB();
         using NpgsqlCommand cmd = 
-            new NpgsqlCommand("INSERT INTO tisch (laenge,breite,hoehe) VALUES (v1,v2,v3)", myConnection);
+            new NpgsqlCommand("INSERT INTO tisch (laenge,breite,hoehe) VALUES (:v1,v2,v3)", myConnection);
         cmd.Parameters.AddWithValue("v1", tisch.laenge);
         cmd.Parameters.AddWithValue("v2", tisch.breite);
         cmd.Parameters.AddWithValue("v3", tisch.hoehe);
